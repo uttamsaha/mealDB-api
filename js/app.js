b8d53403d5c06ce02fe08b92490121c00e148804
UttamSaha <uttamsaha982@gmail.com>
2022-02-27 18:08:54 +0600

    responsive

--- js/app.js
+++ js/app.js
@@ -1,4 +1,15 @@
+//toggle spinner
+const toggleSpinner = displayStyle => {
+    document.getElementById('spinner').style.display = displayStyle;
+}
+//
+const toggleSearchResult = displayStyle => {
+    document.getElementById('result').style.display = displayStyle;
+}
+
 const loadFoods = () => {
+    toggleSpinner('block');
+    toggleSearchResult('none');
     const inputField = document.getElementById('input-field');
     const inputFieldValue = inputField.value;
 
@@ -12,15 +23,18 @@ const loadFoods = () => {
 const displayFoods = foods => {
     // console.log(foods);
     const divContainer = document.getElementById('search-result');
+    divContainer.textContent = '';
     foods.forEach(food => {
         const div = document.createElement('div');
         div.className = 'foods';
         div.innerHTML = `
         <img width="350px" src="${food.strMealThumb}" />
-        <h3>${food.strMeal}</h3>
+        <h5>${food.strMeal}</h5>
         <p>${food.strInstructions.slice(0,80)}</p>
         `
         divContainer.appendChild(div);
         console.log(food);
     })
+    toggleSpinner('none');
+    toggleSearchResult('block');
 }
